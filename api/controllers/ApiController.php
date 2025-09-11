ee095995714482140d4b4a2245dc3c412909d319
Alexey <aleksey_panfilov@bk.ru>
2025-09-11 16:38:13 +0300

    labethernet | api | added create user function, refactored code, added message to response function

--- api/controllers/ApiController.php
+++ api/controllers/ApiController.php
@@ -1,25 +1,56 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT']. '/api/src/Database.php';
-Class ApiController {
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/src/Database.php';
 
-    public static function getUser($id){
-        $sql_query = "SELECT * FROM users WHERE id=?";
-        $params = [$id];
-        $result = Database::execute($sql_query, 'i', $params);
-        for($user = array(); $data = $result->fetch_assoc(); $user[] = $data);
-        echo ApiController::getResponse($user, 200);
+class ApiController
+{
+
+    public static function getUser($id)
+    {
+        if (is_numeric($id)) {
+            $sql_query = "SELECT * FROM users WHERE id=?";
+            $params = [$id];
+            $result = Database::execute($sql_query, 'i', $params);
+            for ($user = array(); $data = $result->fetch_assoc(); $user[] = $data) ;
+            if(count($user)=== 1) $user = array_pop($user);
+            return ApiController::getResponse($user);
+        }
+        return ApiController::getResponse(null, 400, 'id ресурса должен быть числом');
     }
 
-    public static function createUser($login, $pass, $email =''){}
-    public static function updateUser($id, $data){}
-    public static function deleteUser($id){}
+    public static function createUser($login, $pass, $email = '')
+    {
+        $sql_query = "INSERT INTO users (login, password_hash, email) VALUES (?, ?, ?)";
+        $hash_pass = password_hash($pass, PASSWORD_DEFAULT);
+        $params = [$login, $hash_pass, $email];
+        $result = Database::execute($sql_query, 'sss', $params);
+        echo $result->insert_id;
+        return ApiController::getResponse();
+    }
 
-    private static function getResponse ($data, $code){
+    public static function updateUser($id, $data)
+    {
+    }
+
+    public static function deleteUser($id)
+    {
+    }
+
+    public static function login($login, $hash_pass){
+
+    }
+
+    private static function getResponse($data = [], $code = 200, $message = '')
+    {
         $response = ['code' => $code];
-        if(empty($data)){
+        if (empty($data)) {
             $response['data'] = 'empty';
+        } else {
+            $response['data'] = $data;
         }
-        else {$response['data'] = $data;}
+        if (!empty($message)) {
+            $response['message'] = $message;
+        }
+
         return json_encode($response);
     }
 
