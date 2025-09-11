1e96cfea2c9ffc9613e0e563c086ca570291a8b6
Alexey <aleksey_panfilov@bk.ru>
2025-09-11 16:40:15 +0300

    labethernet | api | refactored code, added simple sql query typing

--- api/src/Database.php
+++ api/src/Database.php
@@ -6,23 +6,43 @@ class Database
 
     private static function init()
     {
-        if(!empty(self::$db)) return;
+        if (!empty(self::$db)) return;
         self::$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
         self::$db->set_charset(DB_CHARSET);
         self::$db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
     }
 
-    public static function execute($query, $var_types_string, $params){
+    public static function execute($query, $var_types_string, $params)
+    {
         Database::init();
         $stmt = self::$db->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Prepare failed: " . self::$db->error . " | Query: " . $query);
+        }
         $stmt->bind_param($var_types_string, ...$params);
+        if (!$stmt) {
+            throw new Exception("Bind param failed: " . self::$db->error . " | Query: " . $query);
+        }
         $stmt->execute();
-        $result = $stmt->get_result();
-        self::exit();
-        return $result;
+        if (!$stmt) {
+            throw new Exception("Execute failed: " . self::$db->error . " | Query: " . $query);
+        }
+        $query_type = strtoupper(strtok(trim($query), " "));
+        switch ($query_type) {
+            case('SELECT'):
+                $result = $stmt->get_result();
+                self::exit();
+                return $result;
+            case('INSERT'):
+                $result = $stmt->insert_id;
+                self::exit();
+                return $result;
+        }
     }
-    private static function exit(){
-        if(!empty(self::$db)){
+
+    private static function exit()
+    {
+        if (!empty(self::$db)) {
             self::$db->close();
         }
     }
