1727252f8df91d0a646486ff6df61d540791f53f
Alexey <aleksey_panfilov@bk.ru>
2025-09-11 23:02:09 +0300

    labethernet | api | refactor api controller, added update user method

--- api/controllers/ApiController.php
+++ api/controllers/ApiController.php
@@ -9,7 +9,7 @@ class ApiController
         if (is_numeric($id)) {
             $sql_query = "SELECT * FROM users WHERE id=?";
             $params = [$id];
-            $result = Database::execute($sql_query, 'i', $params);
+            $result = Database::execute($sql_query, 'i', $params, 'SELECT');
             for ($user = array(); $data = $result->fetch_assoc(); $user[] = $data) ;
             if(count($user)=== 1) $user = array_pop($user);
             return ApiController::getResponse($user);
@@ -22,12 +22,52 @@ class ApiController
         $sql_query = "INSERT INTO users (login, password_hash, email) VALUES (?, ?, ?)";
         $hash_pass = password_hash($pass, PASSWORD_DEFAULT);
         $params = [$login, $hash_pass, $email];
-        $result = Database::execute($sql_query, 'sss', $params);
+        $result = Database::execute($sql_query, 'sss', $params, 'INSERT');
         return ApiController::getResponse(['created_id' => $result], 201, 'Создание успешно');
     }
 
-    public static function updateUser($id, $data)
+    public static function updateUser($id, $data, $request_type)
     {
+        $users_columns_list = ['login', 'password_hash', 'email'];
+        $sql_query = "UPDATE users SET ";
+        $params = [];
+        $var_types_string = "";
+        $data['password_hash'] = password_hash($data['password_hash'], PASSWORD_DEFAULT);
+        $update_string = '';
+        if ($request_type == "PATCH"){
+
+            $k = 0;
+            foreach($data as $key => $value){
+                    $k++;
+                    $params[] = $value;
+                    $update_string .= $key . '=?';
+                    $update_string .= $k !== (count($data)) ? ', ' : ' ';
+
+                    if(is_string($value)) $var_types_string .= 's';
+                    elseif(is_numeric($value)) $var_types_string .= 'i';
+            }
+        }elseif($request_type== "PUT"){
+            foreach($users_columns_list as $key => $column){
+                $value = $data[$column];
+                $params[] = empty($value)? null: $value;
+                $update_string .= $column . '=?';
+                $update_string .= $key !== (count($users_columns_list) - 1) ? ', ' : ' ';
+
+                if(is_string($value)) {$var_types_string .= 's';}
+                elseif(is_numeric($value)) $var_types_string .= 'i';
+                elseif(is_null($value)){$var_types_string .= 's';}
+            }
+        }
+        echo is_string($params[0]) ? 'Да' : "Нет";
+        $params[] = $id;
+        $var_types_string .= 'i';
+        $sql_query .= $update_string . "WHERE id=?";
+        echo $sql_query;
+        print_r($params);
+        echo $var_types_string;
+        $affected_rows = Database::execute($sql_query, $var_types_string, $params, 'UPDATE');
+        return $affected_rows > 0 ? ApiController::getResponse($affected_rows, 200, 'Изменение успешно'):
+            ApiController::getResponse($affected_rows, 200, 'Не было изменено ни одной строки');
     }
 
     public static function deleteUser($id)
