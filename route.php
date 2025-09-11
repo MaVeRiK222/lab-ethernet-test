8dd3c8fe5cc85000aab924f73352eecfe3db8776
Alexey <aleksey_panfilov@bk.ru>
2025-09-11 23:01:29 +0300

    labethernet | api | refactor route file, added code to patch and put request methods

--- route.php
+++ route.php
@@ -4,7 +4,8 @@ $url_arr = explode('/', $_GET['q']);
 //print_r($url_arr);
 //echo $_SERVER['REQUEST_METHOD'];
 $request_method = $_SERVER['REQUEST_METHOD'];
-$data = $_REQUEST;
+$raw_data = file_get_contents('php://input');
+$data = json_decode($raw_data, 1);
 switch ($url_arr[0]) {
     case 'api':
         require_once 'api/controllers/ApiController.php';
@@ -18,10 +19,7 @@ switch ($url_arr[0]) {
                 break;
             case "POST":
                 if (empty($url_arr[2])) {
-//                    $data = getRequestData();
-                    $raw_data = file_get_contents('php://input');
-                    $data = json_decode($raw_data, 1);
-//                    $data
+
                     print_r($data);
                     $login = $data['login'];
                     $pass = $data['pass'];
@@ -31,38 +29,35 @@ switch ($url_arr[0]) {
                 }
                 break;
             case "PATCH":
-                if (!empty($url_arr[2])) {
-                    $id = $url_arr[2];
-                    is_numeric($id) ? ApiController::updateUser($id, $data) : null;
-                }
-                break;
+//                if (!empty($url_arr[2])) {
+//                    $id = $url_arr[2];
+//                    is_numeric($id) ? ApiController::updateUser($id, $data) : null;
+//                }
+//                break;
             case "PUT":
                 if (!empty($url_arr[2])) {
                     $id = $url_arr[2];
-                    is_numeric($id) ? ApiController::updateUser($id, $data) : null;
+                    $response = is_numeric($id) ? ApiController::updateUser($id, $data, $request_method) : null;
+                    echo $response;
                 }
+
                 break;
             case "DELETE":
                 if (!empty($url_arr[2])) {
                     $id = $url_arr[2];
-                    is_numeric($id) ? ApiController::deleteUser($id) : null;
+                    $result = is_numeric($id) ? ApiController::deleteUser($id) : null;
+                    echo $result;
                 }
                 break;
             default:
                 response(404, 'Not Found');
         }
-//        ApiController::getUser(2);
         break;
     default:
         response(404, 'Not Found');
 
 }
 
-function getRequestData(){
-    $raw_data = file_get_contents('php://input');
-    $json = json_decode($raw_data);
-    return $json;
-}
 
 function response($code, $message)
 {
