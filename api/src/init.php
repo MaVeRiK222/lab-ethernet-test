19345648eb884e560f25f2705e5bcc27cfd54ab3
Alexey <aleksey_panfilov@bk.ru>
2025-09-14 00:50:49 +0300

    labethernet | api | created logger service

--- api/src/init.php
+++ api/src/init.php
@@ -1,4 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/api/config/db.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/api/src/Validator.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/api/src/Database.php';
+require_once __DIR__ . '\..\config\db.php';
+require_once __DIR__ . '\..\src\Validator.php';
+require_once __DIR__ . '\..\src\Database.php';
+require_once __DIR__ . '\..\..\utils\Logger.php';
