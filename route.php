e321b855ca681f61cac16c19d4d8e3a19ff9525c
Alexey <aleksey_panfilov@bk.ru>
2025-09-14 12:09:02 +0300

    labethernet | api | added validation for put and patch requests, refactor code

--- route.php
+++ route.php
@@ -12,11 +12,11 @@ $request_method = $_SERVER['REQUEST_METHOD'];
 $raw_data = file_get_contents('php://input');
 $data = !empty($raw_data) ? json_decode($raw_data, 1) : null;
 
-$user_token = $_SERVER['HTTP_MY_CUSTOM_TOKEN'];
+$user_token = getallheaders()['my-custom-token'];
 $user_id = $_COOKIE['user_id'];
 
 Logger::log('Запрос от пользователя', 'route.log', $_REQUEST);
-Logger::log('Тип запроса' , 'route.log', $_SERVER['REQUEST_METHOD']);
+Logger::log('Тип запроса', 'route.log', $_SERVER['REQUEST_METHOD']);
 
 switch ($url_arr[0]) {
     case 'api':
@@ -61,7 +61,7 @@ switch ($url_arr[0]) {
                         } else {
                             echo response(404, 'Not Found');
                         }
-                    } catch(Exception $e){
+                    } catch (Exception $e) {
                         Logger::log('Ошибка при создании пользователя', 'route.log', $e->getMessage());
                         response(500, 'Server Error');
                     }
@@ -75,6 +75,17 @@ switch ($url_arr[0]) {
                                 echo response(401, 'Not Authorized');
                                 exit;
                             }
+                            $login = $data['login'] ?? null;
+                            $email = $data['email'] ?? null;
+                            $pass = $data['pass'] ?? null;
+                            $age = $data['age'] ?? null;
+
+                            if (($request_method === "PATCH" && !isPatchValid($email, $login, $pass, $age)) ||
+                            $request_method === "PUT" && !isPutValid($email, $login, $pass, $age)){
+                                response(200, 'Неверные данные');
+                                logEnd();
+                                exit;
+                            }
                             $id = $url_arr[2];
 
                             $response = is_numeric($id) ? ApiController::updateUser($id, $data, $request_method) : null;
@@ -82,7 +93,7 @@ switch ($url_arr[0]) {
                         } else {
                             echo response(404, 'Not Found');
                         }
-                    } catch (Exception $e){
+                    } catch (Exception $e) {
                         Logger::log('Ошибка при обновлении пользователя', 'route.log', $e->getMessage());
                         response(500, 'Server Error');
                     }
@@ -102,7 +113,7 @@ switch ($url_arr[0]) {
                         } else {
                             echo response(404, 'Not Found');
                         }
-                    } catch (Exception $e){
+                    } catch (Exception $e) {
                         Logger::log('Ошибка при удалении пользователя', 'route.log', $e->getMessage());
                         response(500, 'Server Error');
                     }
@@ -119,11 +130,11 @@ switch ($url_arr[0]) {
             require_once __DIR__ . '/api/controllers/ApiController.php';
 
             $data = ApiController::login($data['login'], $data['pass']);
-            if (empty($data)) echo response(200, 'Неверные данные');
-            setcookie('user_id', $data['user_id']);
-            $json = json_encode(['token' => $data['token']]);
-            echo $json;
-        } catch (Exception $e){
+            if (empty($data)) {echo response(200, 'Неверные данные'); exit;}
+            setcookie('user_id', $data['user_id'], time() + 60 * 30, '/');
+            $response = json_encode(['code' => 200, 'token' => $data['token']]);
+            echo $response;
+        } catch (Exception $e) {
             Logger::log('Ошибка при попытке входа', 'route.log', $e->getMessage());
             response(500, 'Server Error');
         }
@@ -140,6 +151,22 @@ function response($code, $message)
 {
     echo $message;
     http_response_code($code);
+}
+
+function isPatchValid($email = null, $login = null, $pass = null, $age = null){
+     return ((Validator::isEmailValid($email) || $email === null) &&
+         (Validator::isLoginValid($login) || $login === null) &&
+         (Validator::isPassValid($pass) || $pass === null) &&
+        (Validator::isAgeValid($age) || $age === null));
+
+}
+
+function isPutValid($email, $login, $pass, $age = null){
+    return (Validator::isEmailValid($email) &&
+        Validator::isLoginValid($login) &&
+        Validator::isPassValid($pass) &&
+        (Validator::isAgeValid($age) || $age == null));
+
 }
 
 function logStart()
